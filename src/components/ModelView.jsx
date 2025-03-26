908f4b86bbad2bf0822e7617865aaa79e0f04d4f
Parth Khandelwal <parthkhandelwal0077@gmail.com>
2025-03-26 23:31:53 +0530

    final commit3

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -3,7 +3,7 @@ import { Html, OrbitControls, PerspectiveCamera, View } from "@react-three/drei"
 import * as THREE from 'three'
 import Lights from './Lights';
 import Loader from './Loader';
-import IPhone from './IPhone';
+import Iphone from './Iphone';
 import { Suspense } from "react";
 
 const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationState, size, item }) => {
@@ -34,7 +34,7 @@ All children inherit these transformations
 Transformations are applied relative to the group's center */}
       <group ref={groupRef} name={`${index === 1} ? 'small' : 'large`} position={[0, 0 ,0]}>
         <Suspense fallback={<Loader />}>
-          <IPhone 
+          <Iphone 
             scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
             item={item}
             size={size}
