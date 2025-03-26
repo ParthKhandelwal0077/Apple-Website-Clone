9966e580a47462c60b9a7da715c96818b3715e83
Parth Khandelwal <parthkhandelwal0077@gmail.com>
2025-03-26 23:21:09 +0530

    final commit

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,20 +1,24 @@
-import { useGSAP } from '@gsap/react'
-import gsap from 'gsap'
-import React from 'react'
-import ModelView from './ModelView'
-import { useState } from 'react'
-import { yellowImg, blueImg, whiteImg, blackImg } from '../utils'
-import * as THREE from 'three'
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap";
+import ModelView from "./ModelView";
+import { useEffect, useRef, useState } from "react";
+import { yellowImg } from "../utils";
+
+import * as THREE from 'three';
+import { Canvas } from "@react-three/fiber";
+import { View } from "@react-three/drei";
+import { models, sizes } from "../constants";
+import { animateWithGsapTimeline } from "../utils/animations";
+
 const Model = () => {
+  const [size, setSize] = useState('small');
+  const [model, setModel] = useState({
+    title: 'iPhone 15 Pro in Natural Titanium',
+    color: ['#8F8A81', '#FFE7B9', '#6F6C64'],
+    img: yellowImg,
+  })
 
-    const [size, setSize] = useState('small');
-    const [model, setModel] = useState({
-      title: 'iPhone 15 Pro in Natural Titanium',
-      color: ['#8F8A81', '#FFE7B9', '#6F6C64'],
-      img: yellowImg,
-    })
-
-      // camera control for the model view
+  // camera control for the model view
   const cameraControlSmall = useRef();
   const cameraControlLarge = useRef();
 
@@ -26,26 +30,100 @@ const Model = () => {
   const [smallRotation, setSmallRotation] = useState(0);
   const [largeRotation, setLargeRotation] = useState(0);
 
-   
-    useGSAP(() => {
-        gsap.to('#heading', { opacity: 1, y: 0, scrollTrigger:{
-            trigger: '#heading',
-    
-        } })
-    })
+  const tl = gsap.timeline();
+
+  useEffect(() => {
+    if(size === 'large') {
+      animateWithGsapTimeline(tl, small, smallRotation, '#view1', '#view2', {
+        transform: 'translateX(-100%)',
+        duration: 2
+      })
+    }
+
+    if(size ==='small') {
+      animateWithGsapTimeline(tl, large, largeRotation, '#view2', '#view1', {
+        transform: 'translateX(0)',
+        duration: 2
+      })
+    }
+  }, [size])
+
+  useGSAP(() => {
+    gsap.to('#heading', { y: 0, opacity: 1 })
+  }, []);
+
   return (
-    <section className='common-padding'>
-        <div className="screen-max-width">
-            <h1 id="heading" className='section-heading'>
-                take a closer look.
-            </h1>
-            <div className='flex flex-col items-center mt-5'>
-                 <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
-                    <ModelView/>
-                 </div>
+    <section className="common-padding">
+      <div className="screen-max-width">
+        <h1 id="heading" className="section-heading">
+          Take a closer look.
+        </h1>
+
+        <div className="flex flex-col items-center mt-5">
+          <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
+            <ModelView 
+              index={1}
+              groupRef={small}
+              gsapType="view1"
+              controlRef={cameraControlSmall}
+              setRotationState={setSmallRotation}
+              item={model}
+              size={size}
+            />  
+
+            <ModelView 
+              index={2}
+              groupRef={large}
+              gsapType="view2"
+              controlRef={cameraControlLarge}
+              setRotationState={setLargeRotation}
+              item={model}
+              size={size}
+            />
+            {/* when you want to have multiple views of the same 3D scene (like showing both small and large models), you need to use the View component from @react-three/drei
+The View components (ModelView) are declared outside the Canvas but are connected to it through the View.Port */}
+
+            <Canvas
+              className="w-full h-full"
+              style={{
+                position: 'fixed',
+                top: 0,
+                bottom: 0,
+                left: 0,
+                right: 0,
+                overflow: 'hidden'
+              }}
+              eventSource={document.getElementById('root')}
+            >
+              <View.Port />
+              {/* View.Port acts as a bridge between the Canvas and the individual Views
+              Have different camera controls for each view
+Maintain independent states for each view while sharing the same 3D scene
+              */}
+            </Canvas>
+          </div>
+
+          <div className="mx-auto w-full">
+            <p className="text-sm font-light text-center mb-5">{model.title}</p>
+
+            <div className="flex-center">
+              <ul className="color-container">
+                {models.map((item, i) => (
+                  <li key={i} className="w-6 h-6 rounded-full mx-2 cursor-pointer" style={{ backgroundColor: item.color[0] }} onClick={() => setModel(item)} />
+                ))}
+              </ul>
+
+              <button className="size-btn-container">
+                {sizes.map(({ label, value }) => (
+                  <span key={label} className="size-btn" style={{ backgroundColor: size === value ? 'white' : 'transparent', color: size === value ? 'black' : 'white'}} onClick={() => setSize(value)}>
+                    {label}
+                  </span>
+                ))}
+              </button>
             </div>
+          </div>
         </div>
-      
+      </div>
     </section>
   )
 }
